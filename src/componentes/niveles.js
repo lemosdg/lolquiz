9b0f049b29c1b4a00148ae51a63602d192acff59
Diego <diegolemosgarcia@gmail.com>
2024-06-15 11:47:49 +0200

    elimino estado nivelActual

--- src/componentes/niveles.js
+++ src/componentes/niveles.js
@@ -5,14 +5,12 @@ import { useNavigate } from 'react-router-dom'
 
 function Niveles() {
   const items = Conexion()
-  const [nivelActual, setNivelActual] = useState(0)
   const { puntuacion } = useContext(AppContext)
   const objetivos = [0, 0, 0]
   const navigate = useNavigate()
 
   const cambiarNivel = (nivel) => {
     if (puntuacion >= objetivos[nivel - 1]) {
-      setNivelActual(nivel)
       const filtrados = items.filter((item) => item.level === nivel)
       navigate('/pregunta', { state: { lista: filtrados } })
     }
