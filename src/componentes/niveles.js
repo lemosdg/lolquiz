6819ebe793b0b26eb94213c7bd51f5c1b4c33e99
Diego <diegolemosgarcia@gmail.com>
2024-06-16 11:40:47 +0200

    niveles.css solo aplica a niveles.js

--- src/componentes/niveles.js
+++ src/componentes/niveles.js
@@ -17,14 +17,28 @@ function Niveles() {
     }
   }
 
+  useEffect(() => {
+    // Al montar el componente
+    document.body.classList.add('estilo-especifico')
+
+    // Al desmontar el componente
+    return () => {
+      document.body.classList.remove('estilo-especifico')
+    }
+  }, [])
+
   return (
     <div className="menuDiv">
       <div className="botonLine">
-        <button onClick={() => cambiarNivel(1)}>Nivel 1</button>
+        <button className="boton" onClick={() => cambiarNivel(1)}>
+          Nivel 1
+        </button>
         <div className="requisitos">{objetivos[0]} necesarios</div>
       </div>
       <div className="botonLine">
-        <button onClick={() => cambiarNivel(2)}>Nivel 2</button>
+        <button className="boton" onClick={() => cambiarNivel(2)}>
+          Nivel 2
+        </button>
         <div className="requisitos">{objetivos[1]} necesarios</div>
       </div>
     </div>
